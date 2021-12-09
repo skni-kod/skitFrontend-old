99ec7ae5230f458ecce474915729553ad51b7c38
Artur Szałyga <arturszalyga@wp.pl>
2021-12-09 10:54:21 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" class="bg-main-gray">
+    <nav class="flex justify-between items-center ml-48 mr-12">
+      
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+#nav {
+  padding: 14px;
+}
+
+#nav a {
+  font-size: 16px;
+}
+
+#nav a.router-link-exact-active {
+  color: #000000
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <section id="home" class="bg-main-gray h-full w-full mt-16 pb-32">
+    
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Home extends Vue {}
+</script>
+
+<style scoped>
+</style>
