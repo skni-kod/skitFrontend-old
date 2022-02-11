1449337079f773b940ddb68437e7e3ce08834c98
Artur Szałyga <arturszalyga@wp.pl>
2022-02-11 14:30:48 +0100

    Successful migration

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{vue, js, ts, jsx, tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "skni-blue-500": "#55ACEE",
+        "skni-blue-600": "#016FA5",
+        "skni-blue-700": "#00699D",
+        "skni-blue-800": "#174b82",
+        "skni-blue-900": "#2E336F",
+      },
+    },
+  },
+  plugins: [],
+};
