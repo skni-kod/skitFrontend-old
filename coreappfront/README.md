8e5174cae9cf5ae2c09564eb19f2381b28019d95
Marcin_Bator <marcinbator.ofc@gmail.com>
2022-10-26 21:33:38 +0200

    Project setup
    React app created
    HTML basic setup

---
+++ coreappfront/README.md
@@ -0,0 +1,3 @@
+COREAPP
+
+Aplikacja stworzona przez Studenckie Koło Naukowe Informatyków "KOD"
---
+++ coreappfront/src/js/App.js
@@ -0,0 +1,8 @@
+import logo from "../svg/logo.svg";
+import "../css/App.css";
+
+function App() {
+  return <>123</>;
+}
+
+export default App;
