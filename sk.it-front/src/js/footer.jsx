e08a23ca8779a4ab72ff62408f379baed503096a
Marcin_Bator <marcinbator.ofc@gmail.com>
2022-11-04 21:08:26 +0100

    footer add

---
+++ sk.it-front/src/js/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../css/static.css";
+
+class Footer extends React.component {
+  render() {
+    return <>sthg</>;
+  }
+}
+
+export default Footer;
--- sk.it-front/src/js/navbar.jsx
+++ sk.it-front/src/js/navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../css/navbar.css";
+import "../css/static.css";
 
 const projectName = "sk.it";
 class Navbar extends React.Component {
