c5f6a267d373bfd962818a5703590c3794121b7b
Marcin Bator <107879757+HeilMartinos@users.noreply.github.com>
2022-11-08 17:42:01 +0100

    Delete sk.it_front2 directory

--- sk.it_front2/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./scss/app.scss";
-import "./scss/static.scss";
-import App from "./ts/app";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
