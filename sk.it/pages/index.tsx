79db8c5b51076501e7bc17c95081f59f44786c91
Marcin_Bator <marcinbator.ofc@gmail.com>
2022-11-08 20:30:57 +0100

    Update index.tsx

--- sk.it/pages/index.tsx
+++ sk.it/pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from "next/head";
 import App from "./_app";
 
-export default function Home() {
+export default function Root() {
   return (
-    <div className="root">
+    <div>
       <Head>
         <title>sk-it</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <App />
