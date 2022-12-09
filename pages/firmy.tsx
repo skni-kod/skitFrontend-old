2a78f188b98f89097dfa1bcea9f03c49b4c630b2
Marcin_Bator <marcinbator.ofc@gmail.com>
2022-12-09 21:02:00 +0100

    arrow scroll

--- pages/firmy.tsx
+++ pages/firmy.tsx
@@ -3,6 +3,8 @@ import { Navbar } from "@/components/navbar/navbar";
 import { Footer } from "@/components/footer/footer";
 import styles from "@/styles/pages.module/companies.module.scss";
 import { VscChevronUp, VscSearch } from "react-icons/vsc";
+import { useCallback, useEffect, useState } from "react";
+import { motion } from "framer-motion";
 export default function Firmy() {
   return (
     <>
@@ -39,14 +41,42 @@ export default function Firmy() {
           <Company />
           <Company />
         </div>
-        <a href="#top">
-          <VscChevronUp className={styles.arrow} />
-        </a>
+        <ScrollButton />
         <Footer />
       </div>
     </>
   );
 }
 function Company() {
-  return <div className={styles.company}>xd</div>;
+  return <div className={styles.company}></div>;
+}
+function ScrollButton() {
+  const [scrollY, setScrollY] = useState(0);
+  const onScroll = useCallback((event: any) => {
+    const { pageYOffset, scrollY } = window;
+    setScrollY(window.pageYOffset);
+  }, []);
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+  if (scrollY > 1000) {
+    return (
+      <div className={styles.arrowUp}>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <a href="#top">
+            <VscChevronUp />
+          </a>
+        </motion.div>
+      </div>
+    );
+  }
+  return <></>;
 }
