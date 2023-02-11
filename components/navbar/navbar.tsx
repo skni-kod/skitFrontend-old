96d4372d9433f996589c0ea00e0b519c66139e7f
Marcinek <marcinbator.ofc@gmail.com>
2023-02-11 16:29:04 +0100

    css mobile main-page

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -2,6 +2,7 @@ import styles from "./navbar.module.scss";
 import Image from "next/image";
 import Logo from "@/public/faviconWhite.png";
 import Link from "next/link";
+import { VscMenu } from "react-icons/vsc";
 
 export function Navbar() {
   return (
@@ -27,6 +28,25 @@ export function Navbar() {
         <Link className={styles.a} href={"./zaloguj"}>
           <li className={styles.li}>Zaloguj</li>
         </Link>
+      </menu>
+      <menu className={styles.mobile_menu}>
+        <div className={styles.menu_icon}>
+          <VscMenu />
+        </div>
+        <div className={styles.mobile_menu_items}>
+          <Link className={styles.a} href={"."}>
+            <li className={styles.li}>Strona główna</li>
+          </Link>
+          <Link className={styles.a} href={"/firmy"}>
+            <li className={styles.li}>Firmy</li>
+          </Link>
+          <Link className={styles.a} href={"./dodajfirme"}>
+            <li className={styles.li}>Dodaj firmę</li>
+          </Link>
+          <Link className={styles.a} href={"./zaloguj"}>
+            <li className={styles.li}>Zaloguj</li>
+          </Link>
+        </div>
       </menu>
     </nav>
   );
